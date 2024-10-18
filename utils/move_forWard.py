f2a2c33f454f257dcee962c0ecafe3f15ab981f9
tfitahiantsoa <gitlab@git.teliae.local>
2024-10-18 10:25:08 +0200

    Refacto v1

--- utils/move_forWard.py
+++ utils/move_forWard.py
@@ -1,11 +1,29 @@
-def move_forward(position, orientation):
-    if orientation == 'N':
+from abc import ABC, abstractmethod
+
+class Orientation(ABC):
+    @abstractmethod
+    def move(self, position):
+        pass
+
+class North(Orientation):
+    def move(self, position):
         position['y'] += 1
-    elif orientation == 'S':
+        return position
+
+class South(Orientation):
+    def move(self, position):
         position['y'] -= 1
-    elif orientation == 'E':
+        return position
+
+class East(Orientation):
+    def move(self, position):
         position['x'] += 1
-    elif orientation == 'W':
+        return position
+
+class West(Orientation):
+    def move(self, position):
         position['x'] -= 1
-    
-    return position
+        return position
+
+def move_forward(position, orientation: Orientation):
+    return orientation.move(position)
