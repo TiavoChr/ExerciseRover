651cb9e2da7592fb39131979ba7ec7566ccaa196
Sachiel <powersosig@riseup.net>
2024-11-26 11:48:02 +0100

    add mission control messenger interface

--- control/mission_control.py
+++ control/mission_control.py
@@ -1,8 +1,7 @@
 # create client socket class (MissionControlClient) to connect to the rover (RoverServer) and send commands to it.
 
-import socket
-import sys
 from abc import ABC, abstractmethod
+from typing import Callable
 
 class IRoverInterpreter(ABC):
     @abstractmethod
@@ -14,20 +13,14 @@ class IMessageDecoder(ABC):
     def decode(message: str) -> str:
         pass
 
-class IMessageEncoder(ABC):
+class ICommandListener(ABC):
     @abstractmethod
-    def encode(command: str) -> str:
+    def subscribe(callback: Callable[[str], RoverState]):
         pass
 
-class IMessenger(ABC):
+class ICommandSender(ABC):
     @abstractmethod
-    def send(self, command: str):
-        pass
-    @abstractmethod
-    def receive(self) -> str:
-        pass
-    @abstractmethod
-    def close(self):
+    def send(command: str) -> RoverState:
         pass
 
 # get command in tcp message "BEGIN:command:END"
@@ -41,68 +34,14 @@ def decode_message_to_command(message: str) -> str:
 
 # @deprecated
 def encode_command_to_message(command: str) -> str:
-    return f'BEGIN:{command}:END'
+    return f'BEGIN:{command}:CRC:END'
 
-class MissionControlClient:
-    def __init__(self, host: str = '127.0.0.1', port: int = 24113):
-        self.host = host
-        self.port = port
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.host, self.port))
+class MissionControl:
+    def __init__(self, commandSender: ICommandSender, rover: IRoverInterpreter):
+        self.__commandSender = commandSender
+        self.__rover = rover
 
-    def send(self, command: str):
-        data = encode_command_to_message(command)
-        self.client.send(data.encode())
-
-    def receive(self) -> str:
-        message = self.client.recv(1024).decode()
-        return decode_message_to_command(message)
-
-    def close(self):
-        self.client.close()
-
-# make RoverServer class to handle the connection from the rover
-
-class RoverServer:
-    def __init__(self, host: str = '127.0.0.1', port: int = 24113):
-        self.host = host
-        self.port = port
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen(1)
-        self.client, self.addr = self.server.accept()
-
-    def send(self, command: str):
-        data = encode_command_to_message(command)
-        self.client.send(data.encode())
-
-    def receive(self) -> str:
-        message = self.client.recv(1024).decode()
-        return decode_message_to_command(message)
-
-    def close(self):
-        self.client.close()
-
-class SocketCommunication(IMessenger):
-    pass
-
-
-# test on main function
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python mission_control.py [client|server]')
-        sys.exit(1)
-
-    if sys.argv[1] == 'client':
-        client = MissionControlClient()
-        client.send('Hello Rover!')
-        print(client.receive())
-        client.close()
-    elif sys.argv[1] == 'server':
-        server = RoverServer()
-        print(server.receive())
-        server.send('Hello Mission Control!')
-        server.close()
-    else:
-        print('Usage: python mission_control.py [client|server]')
-        sys.exit(1)
+    def send(self, command: str) -> RoverState:
+        self.__commandSender.send(command)
+        self.__rover.receive(command)
+        return self.__rover.getState()
